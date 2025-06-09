83b184bb6dfae1196ac9ccd0e04b59d08adc97ef
zbxzsm <zbxsyghr@gmail.com>
2025-06-09 17:02:52 +0800

    Site updated: 2025-06-09 17:02:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/08/这是另一篇新博文/","2025/06/08/这是一篇新的博文/","2025/06/07/hello-world/"];function toRandomPost(){
+var posts=["2025/06/08/这是另一篇新博文/","2025/06/09/测速文章图片/","2025/06/08/这是一篇新的博文/","2025/06/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
